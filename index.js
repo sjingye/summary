9c0350a7f911f43d53aa6a8e94fb022a3e36bee7
sj <songjingjing.a@mininglamp.com>
2020-10-25 23:20:45 +0800

    app

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: sjingye
+ * @Date: 2020-10-25 19:29:34
+ * @LastEditTime: 2020-10-25 23:06:29
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /summary/src/index.js
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import { Switch, BrowserRouter } from "react-router-dom";
+import Routers from './routers'
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routers></Routers>
+    </BrowserRouter>
+  );
+}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
